0471d28fb2d09f45388b009f8d5aade7eefea6ea
BTian8 <boyan.t@wustl.edu>
2022-08-27 12:58:39 -0500

    twitch

---
+++ .history/src/utils_20220827115212.js
@@ -0,0 +1,20 @@
+const SERVER_ORIGIN = '';
+ 
+const loginUrl = `${SERVER_ORIGIN}/login`;
+ 
+export const login = (credential) => {
+  return fetch(loginUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify(credential)
+  }).then((response) => {
+    if (response.status !== 200) {
+      throw Error('Fail to log in');
+    }
+ 
+    return response.json();
+  })
+}
